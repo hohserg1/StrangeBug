0cd594bbae218bf911f3da5b6b6b62316883ad07
GlassSpirit <yurikochkun@gmail.com>
2021-07-04 13:42:17 +0300

    workspace fixes

--- app/src/main/java/hohserg/strange/bug/Main.java
+++ app/src/main/java/hohserg/strange/bug/Main.java
@@ -1,17 +1,25 @@
 package hohserg.strange.bug;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("strange_bug")
 public class Main {
-    public Main() {
-        try {
-            System.out.println("Main successful: " + Class.forName("hohserg.strange.bug.TestCompanion"));
-        } catch (ClassNotFoundException e) {
-            System.out.println("Main failed: ");
-            e.printStackTrace();
-        }
-        ClassFromSubmodule.test();
-        System.out.println();
-    }
+	public Main() {
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onCommonSetup);
+	}
+
+	private void onCommonSetup(FMLCommonSetupEvent event) {
+		try {
+			System.out.println("Classloader for " + getClass().getName() + " is " + getClass().getClassLoader());
+			Class testCompanionClass = Class.forName("hohserg.strange.bug.TestCompanion");
+			System.out.println("Main successful: class " + testCompanionClass.getName() + ", classloader " + testCompanionClass.getClassLoader());
+		} catch (ClassNotFoundException e) {
+			System.out.println("Main failed: ");
+			e.printStackTrace();
+		}
+		ClassFromSubmodule.test();
+		System.out.println();
+	}
 }
--- common/src/main/java/hohserg/strange/bug/ClassFromSubmodule.java
+++ common/src/main/java/hohserg/strange/bug/ClassFromSubmodule.java
@@ -1,12 +1,14 @@
 package hohserg.strange.bug;
 
 public class ClassFromSubmodule {
-    public static void test() {
-        try {
-            System.out.println("ClassFromSubmodule successful: " + Class.forName("hohserg.strange.bug.TestCompanion"));
-        } catch (Throwable e) {
-            System.out.println("ClassFromSubmodule failed: ");
-            e.printStackTrace();
-        }
-    }
+	public static void test() {
+		try {
+			System.out.println("Classloader for " + ClassFromSubmodule.class.getName() + " is " + ClassFromSubmodule.class.getClassLoader());
+			Class testCompanionClass = Class.forName("hohserg.strange.bug.TestCompanion");
+			System.out.println("ClassFromSubmodule successful: class " + testCompanionClass.getName() + ", classloader " + testCompanionClass.getClassLoader());
+		} catch (Throwable e) {
+			System.out.println("ClassFromSubmodule failed: ");
+			e.printStackTrace();
+		}
+	}
 }
